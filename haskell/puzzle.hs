a61b50452098ddabe35730f7accdb793ec948532
Kevin Kredit <k.kredit.us@ieee.org>
2020-02-10 20:25:29 -0500

    Begin Haskell solution

---
+++ haskell/puzzle.hs
@@ -0,0 +1,9 @@
+-- puzzle.hs
+-- Solves a scramble-squares puzzle using idiomatic Haskell
+-- Copyright (c) 2020, Kevin Kredit
+-- License MIT
+
+module Main
+  where
+
+main = putStrLn "Working on a solution!"
