7c27b94d9dac35e7b548f5ebb68d12158fedb29e
Kevin Kredit <k.kredit.us@ieee.org>
2020-03-18 14:43:04 -0400

    Begin Rust solution

---
+++ rust/puzzle.rs
@@ -0,0 +1,8 @@
+// puzzle.rs
+// Solves a scramble-squares puzzle using idiomatic Rust
+// Copyright (c) 2020, Kevin Kredit
+// License MIT
+
+fn main() {
+    println!("Working on a solution!");
+}
