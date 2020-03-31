8995d2acbd3d37564a7f185a6e453a6c5d35b914
Kevin Kredit <k.kredit.us@ieee.org>
2020-03-31 10:33:31 -0400

    Clojure: stylistic fixups

--- clojure/project.clj
+++ clojure/project.clj
@@ -1,8 +1,8 @@
 (defproject puzzle "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "A program to solve a scramle-squares puzzle"
+  :url "https://github.com/kkredit/scramble-squares-solver"
+  :license {:name "MIT License"
+            :url "https://mit-license.org/"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main puzzle.core
   :aot [puzzle.core])
